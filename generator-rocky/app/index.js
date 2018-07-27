02e22b3499bc0263f9283069030d9f6edf53ea9e
johnzhu12 <1042532367@qq.com>
2018-07-27 14:42:25 +0800

    update

--- generator-rocky/app/index.js
+++ generator-rocky/app/index.js
@@ -1,3 +1,4 @@
+
 var Generator = require('yeoman-generator'),
     chalk = require('yeoman-generator/node_modules/chalk'),
     glob = require('yeoman-generator/node_modules/glob'),
@@ -9,12 +10,14 @@ var Generator = require('yeoman-generator'),
 
 
 module.exports = class extends Generator {
+
     // The name `constructor` is important here
     constructor(args, opts) {
         // Calling the super constructor is important so our generator is correctly set up
         super(args, opts);
 
         var dirs = glob.sync('+(rocky-web)');
+
         //now _.contains has been abandoned by lodash,use _.includes
         if (_.includes(dirs, 'rocky-web')) {
             this.log(chalk.bold.green('资源已经初始化，退出...'));
@@ -22,23 +25,26 @@ module.exports = class extends Generator {
                 process.exit(1);
             }, 200);
         }
+        //变量
+        this.a = this.destinationRoot();
+        this.log('根目录', this.a)
+        this.b = this.sourceRoot();
+        this.log('模板目录', this.b)
     }
 
     prompting() {
-        var a = this.destinationRoot();
-        this.log('根目录', a)
-        var b = this.sourceRoot();
-        this.log('模板目录', b)
 
-        var questions = require(b + '/configs/questionsConfig.json')
+
+        var questions = require(this.b + '/configs/questionsConfig.json')
         //项目名字默认写成自己当前创建的文件夹
         var appname = this.appname;
-        _.map(questions, function (item) {
-            if (item.name == 'projectAuthor') {
+        questions = _.map(questions, function (item) {
+            if (item.name == 'projectName') {
                 item.default = appname;
             }
+            return item;
         })
-
+        // console.log('questions', questions)
         return this.prompt(questions).then(
             function (answers) {
                 for (var item in answers) {
@@ -48,7 +54,6 @@ module.exports = class extends Generator {
     }
     writing() {
 
-
         //copy templates
         this.fs.copyTpl(
             this.templatePath(),
@@ -56,24 +61,20 @@ module.exports = class extends Generator {
         );
 
         //package.json dependencies
-        const pkgJson = require(b + '/configs/packageConfig.json')
+        const pkgJson = require(this.b + '/configs/packageConfig.json')
         // Extend or create package.json file in destination path
         this.fs.extendJSON(this.destinationPath('package.json'), pkgJson);
 
-        const babelrcJson = require(b + '/configs/balbelrcConfig.json');
+        const babelrcJson = require(this.b + '/configs/balbelrcConfig.json');
         this.fs.extendJSON(this.destinationPath('.babelrc'), babelrcJson);
 
-        const tsConfigJson = require(b + '/configs/tsConfig.json');
+        const tsConfigJson = require(this.b + '/configs/tsConfig.json');
         this.fs.extendJSON(this.destinationPath('tsconfig.json'), tsConfigJson);
     }
     install() {
         this.npmInstall();
     }
     end() {
-        // this.spawnCommand('node', ['rocky-web/server.js']); //本地写的node脚本
         this.spawnCommand('npm', ['start']);
-        // setTimeout(() => {
-        //     require('child_process').exec('open http://localhost:6000/index.html'); //打开浏览器
-        // }, 2000)
     }
 };
